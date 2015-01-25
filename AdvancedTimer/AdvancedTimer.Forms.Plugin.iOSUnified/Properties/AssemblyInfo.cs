d8f068889001ba5a49cac9fc45b9af637c090498
ufuf <ufuk@mobilist.com.tr>
2015-01-25 21:03:33 +0200

    added unified support

--- AdvancedTimer/AdvancedTimer.Forms.Plugin.iOSUnified/Properties/AssemblyInfo.cs
+++ AdvancedTimer/AdvancedTimer.Forms.Plugin.iOSUnified/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdvancedTimer.Forms.Plugin.Android")]
+[assembly: AssemblyTitle("AdvancedTimer.Forms.Plugin.iOSUnified")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdvancedTimer.Forms.Plugin.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("AdvancedTimer.Forms.Plugin.iOSUnified")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("941be9c7-1af7-4976-b322-80fbf7477904")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -26,5 +32,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
